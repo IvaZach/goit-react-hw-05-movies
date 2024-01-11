27835d6d4cd4716cfaec7ec092d31e65d3e33de3
IvaZach <ivazacherniuk@gmail.com>
2024-01-11 22:48:02 +0300

    Add navlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import Home from "page/Home";
-import NotFound from "page/NotFound";
-import { Routes, Route } from "react-router-dom";
+import Home from 'page/Home';
+import Movies from 'page/Movies';
+import NotFound from 'page/NotFound';
+import { Routes, Route, NavLink } from 'react-router-dom';
 // import Home from "path/to/pages/Home";
 // import About from "path/to/pages/About";
 // import Products from "path/to/pages/Products";
 
 export const App = () => {
-  return (
-    <div>
-
-
+  return (<>
+    <header>
+      <nav>
+        <ul>
+          <li>
+          <NavLink to='/movies'>Home</NavLink>
+          </li>
+          <li>
+          <NavLink to='/movies/:moviesId'>Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     
+
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} /> */}
+        <Route path="/movies" element={<Home />} />
+        <Route path="/movies/:moviesId" element={<Movies />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </>
   );
 };
+
+/* const BASE_URL = "https://api.themoviedb.org/3/";
+const ENDPOINT = "trending/movie/day";
+const API_KEY = "ad597f58480ee2b54bd116c59b3fba0a"; */
