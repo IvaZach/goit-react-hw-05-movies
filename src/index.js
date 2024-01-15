eff816980096df20f8e3d046b5fb7a7bde851150
IvaZach <ivazacherniuk@gmail.com>
2024-01-15 22:05:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename="/goit-react-hw-05-movies">
+    <HashRouter >
       <App />
     </HashRouter>
   </React.StrictMode>
